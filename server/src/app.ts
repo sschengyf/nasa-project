1dde628202698d4339beb103e7bcd4bfdaab8f66
Ivan Cheng <sschengyf@gmail.com>
2023-05-03 21:09:51 +1200

    chore: logs

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import morgan from 'morgan';
 import planetsRouter from './routes/planets/planets.router';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(
     origin: 'http://localhost:3000',
   })
 );
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
